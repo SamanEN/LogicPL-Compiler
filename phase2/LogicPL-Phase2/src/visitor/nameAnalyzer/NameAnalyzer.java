47cd6100ee965910c2be2f81331d77d5dce171a9
Saman Eslami Nazari <samaneslami78@gmail.com>
2023-05-14 23:32:59 +0430

    Complete NameAnalyzer

--- phase2/LogicPL-Phase2/src/visitor/nameAnalyzer/NameAnalyzer.java
+++ phase2/LogicPL-Phase2/src/visitor/nameAnalyzer/NameAnalyzer.java
@@ -2,9 +2,10 @@ package visitor.nameAnalyzer;
 
 import ast.node.Program;
 import ast.node.declaration.*;
-import ast.node.statement.VarDecStmt;
+import ast.node.statement.*;
 import compileError.*;
 import compileError.Name.*;
+import main.grammar.LogicPLParser;
 import symbolTable.SymbolTable;
 import symbolTable.symbolTableItems.*;
 import symbolTable.itemException.ItemAlreadyExistsException;
@@ -55,11 +56,49 @@ public class NameAnalyzer extends Visitor<Void> {
         }
 
         for (var stmt : funcDeclaration.getStatements()) {
-            if(stmt instanceof VarDecStmt) {
-                stmt.accept(this);
-            }
+            stmt.accept(this);
         }
 
+        SymbolTable.pop();
+        return null;
+    }
+
+    @Override
+    public  Void visit(AssignStmt assignStmt) {
+        return null;
+    }
+
+    @Override
+    public Void visit(PredicateStmt predicateStmt) {
+        return null;
+    }
+
+    @Override
+    public Void visit(ImplicationStmt implicationStmt) {
+        SymbolTable currentTable = SymbolTable.top;
+        for(Statement statement : implicationStmt.getStatements()) {
+            statement.accept(this);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ReturnStmt returnStmt) {
+        return null;
+    }
+
+    @Override
+    public Void visit(PrintStmt printStmt) {
+        return null;
+    }
+
+    @Override
+    public Void visit(ForloopStmt forloopStmt) {
+        SymbolTable forTable = new SymbolTable(SymbolTable.top, "forLoop" + forloopStmt.getLine());
+        SymbolTable.push(forTable);
+        for(Statement statement : forloopStmt.getStatements()) {
+            statement.accept(this);
+        }
         SymbolTable.pop();
         return null;
     }
@@ -86,6 +125,19 @@ public class NameAnalyzer extends Visitor<Void> {
             currentTable.put(variableItem);
         } catch (ItemAlreadyExistsException e) {
             nameErrors.add(new VariableRedefinition(varDeclaration.getLine(), varDeclaration.getIdentifier().getName()));
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ArrayDecStmt arrayDecStmt) {
+        var variableItem = new VariableItem(arrayDecStmt.getIdentifier().getName(), arrayDecStmt.getType());
+
+        SymbolTable currentTable = SymbolTable.top;
+        try {
+            currentTable.put(variableItem);
+        } catch (ItemAlreadyExistsException e) {
+            nameErrors.add(new VariableRedefinition(arrayDecStmt.getLine(), arrayDecStmt.getIdentifier().getName()));
         }
         return null;
     }
