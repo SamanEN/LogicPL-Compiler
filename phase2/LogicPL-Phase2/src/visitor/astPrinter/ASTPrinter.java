a2e31ff26759fb4760d7b55c55274a755c6adb72
Saman Eslami Nazari <samaneslami78@gmail.com>
2023-05-14 16:39:11 +0430

    Add visit functions for different nodes of grammar
    Fix typo in ImplicationStmt class

--- phase2/LogicPL-Phase2/src/visitor/astPrinter/ASTPrinter.java
+++ phase2/LogicPL-Phase2/src/visitor/astPrinter/ASTPrinter.java
@@ -41,109 +41,143 @@ public class ASTPrinter extends Visitor<Void> {
 
     @Override
     public Void visit(FuncDeclaration funcDeclaration) {
-        // ToDo
+        messagePrinter(funcDeclaration.getLine(), funcDeclaration.toString());
+        funcDeclaration.getIdentifier().accept(this);
+        for (ArgDeclaration arg : funcDeclaration.getArgs())
+            arg.accept(this);
+        for (Statement statement : funcDeclaration.getStatements())
+            statement.accept(this);
         return null;
     }
 
     @Override
     public Void visit(UnaryExpression unaryExpression) {
-        // ToDo
+        messagePrinter(unaryExpression.getLine(), unaryExpression.toString());
+        unaryExpression.getOperand().accept(this);
         return null;
     }
 
     @Override
     public Void visit(BinaryExpression binaryExpression) {
-        // ToDo
+        messagePrinter(binaryExpression.getLine(), binaryExpression.toString());
+        binaryExpression.getLeft().accept(this);
+        binaryExpression.getRight().accept(this);
         return null;
     }
 
     @Override
     public Void visit(Identifier identifier) {
-        // ToDo
+        messagePrinter(identifier.getLine(), identifier.toString());
         return null;
     }
 
     @Override
     public Void visit(ArrayAccess arrayAccess) {
-        // ToDo
+        messagePrinter(arrayAccess.getLine(), arrayAccess.toString());
+        arrayAccess.getIndex().accept(this);
         return null;
     }
 
     @Override
     public Void visit(FunctionCall functionCall) {
-        // ToDo
+        messagePrinter(functionCall.getLine(), functionCall.toString());
+        functionCall.getUFuncName().accept(this);
+        for (Expression arg : functionCall.getArgs())
+            arg.accept(this);
         return null;
     }
 
     @Override
     public Void visit(QueryExpression queryExpression) {
-        // ToDo
+        messagePrinter(queryExpression.getLine(), queryExpression.toString());
+        queryExpression.getPredicateName().accept(this);
+        queryExpression.getVar().accept(this);
         return null;
     }
 
     @Override
     public Void visit(IntValue value) {
-        // ToDo
+        messagePrinter(value.getLine(), value.toString());
         return null;
     }
 
     @Override
     public Void visit(FloatValue value) {
-        // ToDo
+        messagePrinter(value.getLine(), value.toString());
         return null;
     }
 
     @Override
     public Void visit(BooleanValue value) {
-        // ToDo
+        messagePrinter(value.getLine(), value.toString());
         return null;
     }
 
     @Override
     public Void visit(ArrayDecStmt arrayDecStmt) {
-        // ToDo
+        messagePrinter(arrayDecStmt.getLine(), arrayDecStmt.toString());
+        arrayDecStmt.getIdentifier().accept(this);
+        for (Expression expr : arrayDecStmt.getInitialValues())
+            expr.accept(this);
         return null;
     }
 
     @Override
     public Void visit(ForloopStmt forloopStmt) {
-        // ToDo
+        messagePrinter(forloopStmt.getLine(), forloopStmt.toString());
+        forloopStmt.getIterator().accept(this);
+        forloopStmt.getArrayName().accept(this);
+        for (Statement statement : forloopStmt.getStatements())
+                statement.accept(this);
         return null;
     }
 
     @Override
     public Void visit(ImplicationStmt implicationStmt) {
-        // ToDo
+        messagePrinter(implicationStmt.getLine(), implicationStmt.toString());
+        implicationStmt.getCondition().accept(this);
+        for (Statement statement : implicationStmt.getStatements())
+            statement.accept(this);
         return null;
     }
 
     @Override
     public Void visit(PredicateStmt predicateStmt) {
-        // ToDo
+        messagePrinter(predicateStmt.getLine(), predicateStmt.toString());
+        predicateStmt.getIdentifier().accept(this);
+        predicateStmt.getVar().accept(this);
         return null;
     }
 
     @Override
     public Void visit(ReturnStmt returnStmt) {
-        // ToDo
+        messagePrinter(returnStmt.getLine(), returnStmt.toString());
+        if (returnStmt.getExpression() != null)
+            returnStmt.getExpression().accept(this);
         return null;
     }
 
     @Override
     public Void visit(VarDecStmt varDecStmt) {
-        // ToDo
+        messagePrinter(varDecStmt.getLine(), varDecStmt.toString());
+        varDecStmt.getIdentifier().accept(this);
+        if (varDecStmt.getInitialExpression() != null)
+            varDecStmt.getInitialExpression().accept(this);
         return null;
     }
 
     @Override
     public Void visit(PrintStmt printStmt) {
-        // ToDo
+        messagePrinter(printStmt.getLine(), printStmt.toString());
+        printStmt.getArg().accept(this);
         return null;
     }
 
     @Override
     public Void visit(AssignStmt assignStmt) {
-        // ToDo
+        messagePrinter(assignStmt.getLine(), assignStmt.toString());
+        assignStmt.getLValue().accept(this);
+        assignStmt.getRValue().accept(this);
         return null;
     }
 
